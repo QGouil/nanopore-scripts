832842f79d585975778e71dbcc179df12f947c50
QGouil <kent1gouil@gmail.com>
2017-08-10 09:45:03 +1000

    Add files via upload

---
+++ nanopolish_methylation_clean.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=2,mem=64gb
+
+module load nanopolish
+set -x
+FASTA=$1
+TMP_DIR=$2
+FMT=$(echo $FASTA | sed 's/.*\.//')
+SCRIPTS_DIR=$PBS_O_HOME/wehisan_home/scripts/quentin/nanopore-scripts
+
+# pull out the header
+HEADER=$TMP_DIR/nanopolish.methylation.header.tsv
+head -n1 $TMP_DIR/$(basename $FASTA).1.${FMT}.methylation.tsv > $HEADER
+BINARY_HEADER=$TMP_DIR/nanopolish.methylation.header.with_binary.tsv
+sed 's/$/\tmethylated/' $HEADER > $BINARY_HEADER
+
+# concatenate files
+$SCRIPTS_DIR/merge_tsv.sh $TMP_DIR/$(basename $FASTA).*.${FMT}.methylation.tsv > ${FASTA}.methylation.tsv
+
+tail -n +2 ${FASTA}.methylation.tsv | awk '{ if (sqrt($5^2) > 2) { print; } }' | cat $HEADER - > ${FASTA}.methylation.filtered.tsv
+
+# add + and - for bismark-style viewing in seqmonk
+tail -n +2 ${FASTA}.methylation.tsv | awk '{ if ($5 > 0) { print $0 "\t+"; } else { print $0 "\t-"; } }' | cat $BINARY_HEADER - > ${FASTA}.methylation.with_binary.tsv &
+tail -n +2 ${FASTA}.methylation.filtered.tsv | awk '{ if ($5 > 0) { print $0 "\t+"; } else { print $0 "\t-"; } }' | cat $BINARY_HEADER - > ${FASTA}.methylation.filtered.with_binary.tsv &
+
+# summarise methylation at genomic locations
+python $NANOPOLISH_SCRIPTS/calculate_methylation_frequency.py -c 0 -i ${FASTA}.methylation.tsv > ${FASTA}.methylation.summary.tsv || echo "ERROR: nanopolish calculate_methylation_frequency.py failed. Check for memory failure" &
+python $NANOPOLISH_SCRIPTS/calculate_methylation_frequency.py -c 2 -i ${FASTA}.methylation.filtered.tsv > ${FASTA}.methylation.filtered.summary.tsv || echo "ERROR: nanopolish calculate_methylation_frequency.py failed. Check for memory failure" &
+wait
+echo "COMPLETE"
+
---
+++ nanopolish_methylation_run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -q small
+#PBS -m abe
+#PBS -M gouil.q@wehi.edu.au
+
+# Usage: qsub -F "path/to/genome.fasta absolute_or_relative_path/to/reads.fastq" nanopolish_methylation_run.sh
+# point to the genome stored on torquelord. Give the full extension. The bwa mem index should be .fa.pac etc.
+# the reads will be in fasta folder.
+# make sure to call that in the AGRF folder?
+
+
+set -x
+cd $PBS_O_WORKDIR
+GENOME=$(realpath $1)
+FASTA=$(realpath $2)
+FMT=$(echo $FASTA | sed 's/.*\.//')
+N=10000
+TMP_DIR="$PBS_O_HOME/tmp/$(dirname $FASTA)"
+SCRIPTS_DIR=$PBS_O_HOME/wehisan_home/scripts/quentin/nanopore-scripts
+
+mkdir -p $TMP_DIR
+cd $TMP_DIR
+if [ ! -f $(basename $FASTA).1.$FMT ]; then
+  python $SCRIPTS_DIR/split_fasta.py $FASTA $N $FMT
+fi
+ARRAY_ID=$(qsub -F "$GENOME $FASTA $TMP_DIR" -t 1-$(ls -1 $TMP_DIR/$(basename $FASTA).*.$FMT | wc -l) $SCRIPTS_DIR/nanopolish_methylation.sh)
+qsub -W "depend=afteranyarray:$ARRAY_ID" -F "$FASTA $TMP_DIR" $SCRIPTS_DIR/nanopolish_methylation_clean.sh
+
