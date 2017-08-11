1097393c6d411e332e1965557120aa0f2b35cbc6
Quentin Gouil <kent1gouil@gmail.com>
2017-08-11 12:25:13 +1000

    produce merged bam from methylation_run.sh

--- nanopolish_methylation_clean.sh
+++ nanopolish_methylation_clean.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=2,mem=64gb
+#PBS -m abe
+#PBS -M gouil.q@wehi.edu.au
 
 module load nanopolish
+module load samtools
+
 set -x
 FASTA=$1
 TMP_DIR=$2
@@ -27,5 +31,11 @@ tail -n +2 ${FASTA}.methylation.filtered.tsv | awk '{ if ($5 > 0) { print $0 "\t
 python $NANOPOLISH_SCRIPTS/calculate_methylation_frequency.py -c 0 -i ${FASTA}.methylation.tsv > ${FASTA}.methylation.summary.tsv || echo "ERROR: nanopolish calculate_methylation_frequency.py failed. Check for memory failure" &
 python $NANOPOLISH_SCRIPTS/calculate_methylation_frequency.py -c 2 -i ${FASTA}.methylation.filtered.tsv > ${FASTA}.methylation.filtered.summary.tsv || echo "ERROR: nanopolish calculate_methylation_frequency.py failed. Check for memory failure" &
 wait
+
+#merge the split bam files into one single bam
+samtools merge ${FASTA}.sorted.bam $TMP_DIR/$(basename $FASTA).*.${FMT}.sorted.bam || echo "ERROR: samtools merge failed"
+samtools index ${FASTA}.sorted.bam
+
+
 echo "COMPLETE"
 
