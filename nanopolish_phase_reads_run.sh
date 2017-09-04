ddee7e5c737635a20539b12266b8176b94d531ab
QGouil <kent1gouil@gmail.com>
2017-09-04 15:14:45 +1000

    map to masked genome if not already done

--- nanopolish_phase_reads_run.sh
+++ nanopolish_phase_reads_run.sh
@@ -4,7 +4,7 @@
 #PBS -M gouil.q@wehi.edu.au
 
 
-# Usage: qsub -F "/path/to/genome.fasta /path/to/reads.fasta /path/to/variants.vcf" nanopolish_phase_reads_run.sh
+# Usage: qsub -F "/path/to/masked_genome.fasta /path/to/unmasked_genome.fasta /path/to/reads.fasta /path/to/variants.vcf" nanopolish_phase_reads_run.sh
 #
 # NB: nanopolish relies on being able to access the fast5 files that created your fastq.
 # /path/to/reads.fasta (or fastq) must be generated using either `poretools` or `nanopolish extract`
@@ -14,9 +14,10 @@
 
 set -x
 cd $PBS_O_WORKDIR
-GENOME=$(realpath $1)
-FASTA=$(realpath $2)
-VCF=$(realpath $3)
+MASKED_GENOME=$(realpath $1)
+UNMASKED_GENOME=$(realpath $2)
+FASTA=$(realpath $3)
+VCF=$(realpath $4)
 if [ $(echo $FASTA | grep -c -e "a$") -gt 0 ]; then
   FMT="fasta"
 elif [ $(echo $FASTA | grep -c -e "q$") -gt 0 ]; then
@@ -35,6 +36,6 @@ if [ ! -f $(basename $FASTA).1.$FMT ]; then
 	python $SCRIPTS_DIR/split_fasta.py $FASTA $N
 	echo "splitting the reads for parallelisation"
 fi
-ARRAY_ID=$(qsub -F "$GENOME $FASTA $VCF $TMP_DIR" -t 1-$(ls -1 $TMP_DIR/$(basename $FASTA).*.$FMT | wc -l) $SCRIPTS_DIR/nanopolish_phase_reads.sh)
+ARRAY_ID=$(qsub -F "$MASKED_GENOME $UNMASKED_GENOME $FASTA $VCF $TMP_DIR" -t 1-$(ls -1 $TMP_DIR/$(basename $FASTA).*.$FMT | wc -l) $SCRIPTS_DIR/nanopolish_phase_reads.sh)
 qsub -W "depend=afteranyarray:$ARRAY_ID" -F "$FASTA $TMP_DIR" $SCRIPTS_DIR/nanopolish_phase_reads_clean.sh
 
